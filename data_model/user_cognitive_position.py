0d1218167126a6ea685bc9bf1045360700bc76dc
Thorsten Zylowski <zyth0001@ads.hs-karlsruhe.de>
2022-10-25 10:07:37 +0200

    attributes for user cognitive position attributes for user goal

--- data_model/user_cognitive_position.py
+++ data_model/user_cognitive_position.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from data_model.educational_graph_node import EducationalGraphNode
+from data_model.learning_object import LearningObject
+from data_model.educational_path import EducationalPath
+
 class UserCognitivePosition(BaseModel):
-    currentState: str
+    formalPosition: EducationalGraphNode
+    latestLearningObject: LearningObject
+    historyOfEducation: EducationalPath
--- data_model/user_goal.py
+++ data_model/user_goal.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from data_model.educational_graph_node import EducationalGraphNode
+from data_model.learning_object import LearningObject
+
 class UserGoal(BaseModel):
-    goalDescription: str # TODO
+    formalGoal: EducationalGraphNode
+    specificGoal: LearningObject
